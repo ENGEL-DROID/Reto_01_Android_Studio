e46763e968a6af371982b41800f4c6aaec342fb0
Iñigo <inigo.campoji@elorrieta-errekamari.com>
2021-10-26 14:25:20 +0200

    Creada primera pantalla#2

--- app/src/main/java/com/example/reto_01_android/MainActivity.java
+++ app/src/main/java/com/example/reto_01_android/MainActivity.java
@@ -2,16 +2,52 @@ package com.example.reto_01_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // GIT TOKEN: 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
     // NEW TOKEN: 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
 
+
+    private EditText Usuario;
+    private EditText Password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Usuario=(EditText) findViewById(R.id.Usuario);
+        Password=(EditText) findViewById(R.id.Password);
     }
+
+    public void entrar(View v) {
+        String usu=Usuario.getText().toString();
+        String pass=Password.getText().toString();
+        SharedPreferences preferencias=getSharedPreferences("agenda",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=preferencias.edit();
+        editor.putString(usu, pass);
+        editor.commit();
+        Toast.makeText(this,"Datos grabados", Toast.LENGTH_LONG).show();
+    }
+    public void recuperar(View v) {
+        String usu=Usuario.getText().toString();
+        SharedPreferences prefe=getSharedPreferences("agenda",
+                Context.MODE_PRIVATE);
+        String d=prefe.getString(usu, "");
+        if (d.length()==0) {
+            Toast.makeText(this,"No existe dicho nombre en la agenda",
+                    Toast.LENGTH_LONG).show();
+        }
+        else {
+            Password.setText(d);
+        }
+    }
+
 }
