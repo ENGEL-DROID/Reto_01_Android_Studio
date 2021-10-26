452c3845e9191bb48ff242f2394fb972285643b2
Iñigo <inigo.campoji@elorrieta-errekamari.com>
2021-10-26 14:09:24 +0200

    Creada primera pantalla

--- app/src/main/java/com/example/reto_01_android/MainActivity.java
+++ app/src/main/java/com/example/reto_01_android/MainActivity.java
@@ -3,43 +3,15 @@ package com.example.reto_01_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     // GIT TOKEN: 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
     // NEW TOKEN: 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
 
-    private EditText txtNombre, txtDescripcion, txtFecha, txtCoste;
-    private Button btnRegistrar, btnCancelar;
-    private CheckBox chHecha;
-    private Spinner spinPrioridad;
-    private String nombre, descripcion, fecha, coste, prioridad;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        txtNombre = findViewById(R.id.txtNombre);
-        txtDescripcion = findViewById(R.id.txtDescripcion);
-        txtFecha = findViewById(R.id.txtFecha);
-        txtCoste = findViewById(R.id.txtCoste);
-        btnRegistrar = findViewById(R.id.btnRegistrar);
-        btnCancelar = findViewById(R.id.btnCancelar);
-        chHecha = findViewById(R.id.chHecha);
-        spinPrioridad = findViewById(R.id.spinPrioridad);
     }
-
-    public void registrar(View v){
-
-    }
-
-
-
-
 }
