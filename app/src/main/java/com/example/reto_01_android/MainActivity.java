41c752e3360eb92189112f3ad5df798f93b96021
Engelbert Restrepo Botello <restrepoengelberth@gmail.com>
2021-10-26 12:23:27 +0200

    Token

--- app/src/main/java/com/example/reto_01_android/MainActivity.java
+++ app/src/main/java/com/example/reto_01_android/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // GIT TOKEN: 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
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
